5b6ea1eb74de1a4daac5ef886967b7f3f80c205b
Ibai Varela <varelaibai@gmail.com>
2024-01-23 18:16:32 +0100

    Añadimos el metodo 1

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        //llamada a metodo 2
+        method2();
     }
 
     public static void method(){
         System.out.println("Esto es un método");
 
+    }
+    public static voi method2(){
+        System.out.println("Otro método");
     }
 }
